4bccb1e08df25d7579aff5b196bb37cf21c0288b
sydykov77 <erjansydykov77>
2020-01-30 22:45:40 +0630

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
 import javax.naming.Name;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         String[] name = {"Ulan", "Umar", "Bulan"};
+
         for (int i = 0; i <= name.length; i++) {
             switch (i) {
                 case 1:
@@ -17,12 +21,15 @@ public class Main {
                 case 3:
                     System.out.println("Good night," + name[2] + "!");
                     break;
-
+                case 4:
+                    System.out.println("Good Job," + name[3]);
+                    break;
             }
 
-        }
+            name = Arrays.copyOf(name, name.length + 1);
+            name[3] = "Urmat";
 
-        
+        }
     }
 
 }
